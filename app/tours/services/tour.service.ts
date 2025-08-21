b51685e96ddbcb712a857c408c5ca9a4e5583c88
Stajara43 <dejanstajic887@gmail.com>
2025-08-21 20:44:44 +0200

    Odradjen pocetni tour services

--- app/tours/services/tour.service.ts
+++ app/tours/services/tour.service.ts
@@ -1,9 +1,23 @@
+import { Tour } from "../models/tour.model";
 export class TourServices {
   private serverUrl = 'http://localhost:48696/';
-  private endpoint = 'api/tours/';
+  private endpoint = 'api/tours';
+  
+  getAllTours(guideId:number,page:number,pageSize:number,orderBy:Text,orderDirection:Text):void{
+      fetch(`${this.serverUrl}${this.endpoint}?guideId=${guideId}&page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&orderDirection=${orderDirection}`)
+      .then(response=>{
+        if(!response.ok)
+        {
+          throw new Error('Request failed. Status: '+response.status)
+        }
+        return response.json();
+      })
+      .then(tours => console.log('Retrived tour:', tours))
+      .catch(error=>console.error('Error:', error.message));
 
+  }
   getTourById(id: number): void {
-    fetch(this.serverUrl + this.endpoint + id)
+    fetch(this.serverUrl + this.endpoint + "/" +id)
       .then(response => {
         if (!response.ok) {
           throw new Error('Request failed. Status: ' + response.status);
@@ -13,4 +27,59 @@ export class TourServices {
       .then(tour => console.log('Retrieved tour:', tour)) // <-- log here
       .catch(error => console.error('Error:', error.message));
   }
+  createTour(newTour:Tour):void{
+    fetch(this.serverUrl + this.endpoint, {
+      method:"POST",
+      headers: { "Content-Type": "application/json" },
+      body:JSON.stringify(newTour)
+    })
+    .then(response => {
+      if(!response.ok)
+      {
+        throw new Error('Request failed. Status: '+response.status);
+      }
+      return response.json();
+    })
+    .then(newTour => {
+        console.log("New tour createdL: ",newTour);
+      }
+    )
+    .catch(error => console.error('Error:', error.message));
+    
+    
+  }
+  updateTour(tourId:number,updatedTour:Tour):void{
+    fetch(this.serverUrl+this.endpoint+'/'+tourId,{
+      method:"PUT",
+      headers: {"Content-Type":"application/json"},
+      body:JSON.stringify(updatedTour)
+    })
+    .then(response=>{
+      if(!response.ok)
+      {
+        throw new Error('Request failed. Status: '+response.status);
+      }
+      return response.json();
+    })
+    .then(updatedTour=>{console.log("Updated tour: ",updatedTour)})
+    .catch(error => console.error('Error', error.message));
+  }
+  deleteTour(tourId:number)
+  {
+    fetch(this.serverUrl+this.endpoint+'/'+tourId,{
+      method:"DELETE",
+      headers: {"Content-Type":"application/json"}
+    })
+    .then(response=>{
+      if(!response.ok)
+      {
+        throw new Error('Request failed. Status: '+response.status);
+      }
+      console.log("Tour deleted");
+    })
+    
+    .catch(error=>console.error('Error', error.message));
+  }
+  
+  //TODO: Get All Tours
 }
